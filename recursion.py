43e6d3f0702cbaf3ca66e1d445048729dc6e2d55
fiyin <lolafiyin@gmail.com>
2025-01-20 20:29:28 +0100

    added iterative factorial

--- recursion.py
+++ recursion.py
@@ -1,5 +1,10 @@
 ## create a python function to practice recursion
 
+## example 
+# 4! = 4 *3 * 2 * 1 = 24
+# 3! = 3 * 2 * 1 = 6
+# 2! = 2 * 1 = 2
+
 def factorial(n):
     if n == 0:
         return 1
@@ -8,3 +13,20 @@ def factorial(n):
 
 
 factorial(10)
+
+# iterative factorial
+def iterative_factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+for i in range(5):
+    print(i, iterative_factorial(i))
+
+# OUTPUT
+# 0 1
+# 1 1
+# 2 2
+# 3 6
+# 4 24
